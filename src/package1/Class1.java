ca4b88aa7341c14aa0a8e0161b7f9597adf18f55
yamanozkan <yamanozkan91@gmail.com>
2021-02-27 22:42:20 +0300

    third commit

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,6 +3,10 @@ package package1;
 public class Class1 {
 	public static void main(String[] args) {
 		
+		System.out.println("Hello World");
+		int numberOfStudents =71;
+		double num = 34.4;
+		
 	}
 
 }
